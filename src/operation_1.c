6b1b9698cfe5cd72e89677bc9b1c8aab654ca4fa
Hellom Oliveira Mendes <heolivei@c1r13s1.42porto.com>
2023-05-11 21:40:12 +0100

    operacoes basicas concluidas

--- src/operation_1.c
+++ src/operation_1.c
@@ -6,7 +6,7 @@
 /*   By: heolivei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 17:52:40 by heolivei          #+#    #+#             */
-/*   Updated: 2023/05/10 20:50:31 by heolivei         ###   ########.fr       */
+/*   Updated: 2023/05/11 21:36:15 by heolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,4 +35,43 @@ void    ft_push(t_stack **dst, t_stack **src)
         *dst = *src;
         *src = (*src)->next;
         (*dst)->next = tmp;
+}
+
+void    ft_rotate(t_stack **head)
+{
+        t_stack *tmp;
+
+        if(!*head || !(*head)->next)
+                return ;
+
+        tmp = *head;
+        *head = ft_end_node(*head);
+        (*head)->next = tmp;
+        *head = tmp->next;
+        tmp->next = NULL;
+}
+
+void    ft_rev_rotate(t_stack **head)
+{
+        t_stack *tmp;
+        int     i;
+
+        if(!*head || !(*head)->next)
+                return ;
+
+        i = 0;
+        tmp = *head;
+        while ((*head)->next)
+        {
+                *head = (*head)->next;
+                i++;
+        }
+
+        (*head)->next = tmp;
+        while (i > 1)
+        {
+                tmp = tmp->next;
+                i--;
+        }
+        tmp->next = NULL;
 }
--- src/operation_2.c
+++ src/operation_2.c
@@ -6,7 +6,7 @@
 /*   By: heolivei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 18:03:15 by heolivei          #+#    #+#             */
-/*   Updated: 2023/05/10 20:55:30 by heolivei         ###   ########.fr       */
+/*   Updated: 2023/05/11 21:39:01 by heolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,4 +45,52 @@ void    ft_pb(t_stack **src, t_stack **dst)
 {
         ft_push(dst, src);
         write(1, "pb\n", 3);
+}
+
+/*---------------------------------------------------------*/
+
+void    ft_ra(t_stack **a)
+{
+        ft_rotate(a);
+
+        write(1, "ra\n", 3);
+}
+
+void    ft_rb(t_stack **b)
+{
+        ft_rotate(b);
+
+        write(1, "rb\n", 3);
+}
+
+void    ft_rr(t_stack **a, t_stack **b)
+{
+        ft_rotate(a);
+	ft_rotate(b);
+
+        write(1, "rr\n", 3);
+}
+
+/*-------------------------------------------------------------*/
+
+void    ft_rra(t_stack **a)
+{
+        ft_rev_rotate(a);
+
+        write(1, "rra\n" , 4);
+}
+
+void    ft_rrb(t_stack **b)
+{
+        ft_rev_rotate(b);
+
+        write(1, "rrb\n" , 4);
+}
+
+void    ft_rrr(t_stack **a, t_stack **b)
+{
+        ft_rev_rotate(a);
+        ft_rev_rotate(b);
+
+        write(1, "rrr\n" , 4);
 }
